438e177007257bcec1791bd6c4b28b2c3fd26170
Jakub Janik <jakubjanik00@gmail.com>
2020-10-19 09:29:07 +0200

    Set up Tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
-module.exports = {
-  /* Your site config here */
-  plugins: [],
+module.exports = {/* Your site config here */
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-postcss',
+      options: {
+        postCssPlugins: [require('tailwindcss')('./tailwind.config.js')]
+      }
+    }
+  ],
 }
