891599908db9a762b08a2d19d49021a82349c51b
Jakub Janik <jakubjanik00@gmail.com>
2020-10-20 08:30:44 +0200

    Create the dark mode toggle in the header

--- src/components/layout.js
+++ src/components/layout.js
@@ -57,6 +57,19 @@ export default function Layout() {
           Kontakt
         </Link>
       </nav>
+
+      <hr className="w-full mt-4 mb-6 border-solid border-green-600" />
+
+      <div className="flex flex-col items-center">
+        <div className="flex items-center gap-1 font-bold text-sm mb-4">
+          <FaAdjust /> Tryb ciemny
+        </div>
+
+        <label className="border-green-300 w-16 h-8 border-2 flex rounded-2xl cursor-pointer" >
+          <input type="checkbox" hidden onChange={e => e.target.nextSibling.classList.toggle('translate-x-full')} />
+          <div className="w-1/2 h-full bg-green-300 rounded-2xl transform transition-transform ease-in duration-500 border-green-500 border-2"></div>
+        </label>
+      </div>
     </header>    
   )
 }
