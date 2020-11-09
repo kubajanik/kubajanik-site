47e0af90966c3167ea2ed663b3d864d9907b0c8c
Jakub Janik <jakubjanik00@gmail.com>
2020-11-09 10:07:16 +0100

    Update contact page
    Add form validation and improve responsiveness.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import Layout from '../components/layout'
 import {FaGithubAlt, FaLinkedinIn, FaStackOverflow} from 'react-icons/fa'
+import {useForm} from 'react-hook-form'
 
 export default function Contact() {
+  const {handleSubmit, register, errors} = useForm()
+  const onSubmit = () => {}
+
   return (
     <Layout>
       <section className="py-12 bg-gray-100">
@@ -45,26 +49,50 @@ export default function Contact() {
         </div>
       </section>
       <section className="py-12">
-        <form className="mx-auto max-w-3xl px-8 grid grid-cols-2 gap-4">
+        <form className="mx-auto max-w-3xl px-8 grid grid-cols-2 gap-4" onSubmit={handleSubmit(onSubmit)}>
           <h3 className="text-center font-bold text-3xl col-span-2">
             Skontaktuj się
           </h3>
 
-          <input
-            className="h-10 p-3 text-gray-600 rounded border border-gray-400 border-solid placeholder-gray-600 focus:border-green-500 focus:outline-none transition-colors duration-500"
-            placeholder="Imię"
-          />
-          <input
-            className="h-10 p-3 text-gray-600 rounded border border-gray-400 border-solid placeholder-gray-600 focus:border-green-500 focus:outline-none transition-colors duration-500"
-            placeholder="Email"
-          />
-          <textarea
-            className="p-3 col-span-2 text-gray-600 rounded border border-gray-400 border-solid placeholder-gray-600 focus:border-green-500 focus:outline-none transition-colors duration-500"
-            rows="10"
-            placeholder="Wprowadź twoją wiadomość"
-          ></textarea>
+          <div className="flex flex-col col-span-2 md:col-span-1">
+            <input
+              className="h-10 p-3 text-gray-600 rounded border border-gray-400 border-solid placeholder-gray-600 focus:border-green-500 focus:outline-none transition-colors duration-500"
+              placeholder="Imię"
+              name="name"
+              ref={register({ required: 'Wprowadź swoje imię' })}
+            />
+            {errors.name && <div className="text-sm text-red-500 py-2">{errors.name.message}</div>}
+          </div>
+
+          <div className="flex flex-col col-span-2 md:col-span-1">
+            <input
+              className="h-10 p-3 text-gray-600 rounded border border-gray-400 border-solid placeholder-gray-600 focus:border-green-500 focus:outline-none transition-colors duration-500"
+              placeholder="Email"
+              name="email"
+              ref={register({
+                required: 'Wprowadź swojego maila',
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: 'Wprowadź poprawny adres email'
+                }
+              })}
+            />
+            {errors.email && <div className="text-sm text-red-500 py-2">{errors.email.message}</div>}
+          </div>
+
+          <div className="flex flex-col col-span-2 ">
+            <textarea
+              className="p-3 text-gray-600 rounded border border-gray-400 border-solid placeholder-gray-600 focus:border-green-500 focus:outline-none transition-colors duration-500"
+              rows="10"
+              placeholder="Wprowadź twoją wiadomość"
+              name="message"
+              ref={register({ required: 'Wprowadź twoją wiadomość' })}
+            ></textarea>
+            {errors.message && <div className="text-sm text-red-500 py-2">{errors.message.message}</div>}
+          </div>
+
           <button
-            className="h-10 p-3 leading-none bg-green-500 rounded col-span-2 text-white font-bold hover:bg-green-700 transition-colors duration-500"
+            className="h-10 p-3 leading-none bg-green-500 rounded col-span-2 text-white font-bold hover:bg-green-700 focus:bg-green-700 focus:outline-none transition-colors duration-500"
             type="submit"
           >
             Wyślij
