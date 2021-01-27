851339bc90538c5048eeeb3e9794f44f61cd8314
Jakub Janik <jakubjanik00@gmail.com>
2021-01-27 20:09:53 +0100

    Fix dark mode toggle

--- src/components/layout/dark-mode.js
+++ src/components/layout/dark-mode.js
@@ -4,6 +4,11 @@ import useDarkMode from 'use-dark-mode'
 
 export default function DarkMode() {
   const darkMode = useDarkMode(false)
+  const [isClient, setIsClient] = React.useState(false)
+
+  React.useEffect(() => {
+    setIsClient(true)
+  }, [])
   
   return (
     <div className="flex flex-col items-center">
@@ -11,14 +16,16 @@ export default function DarkMode() {
         <FaAdjust className="mr-1" /> Tryb ciemny
       </div>
 
-      <label className="border-green-300 dark:border-green-500 w-16 h-8 border-2 flex rounded-2xl cursor-pointer" >
-        <input 
-          type="checkbox" 
-          hidden
-          onChange={darkMode.toggle} 
-        />
-        <div className={`${darkMode.value ? 'translate-x-full' : ''} w-1/2 h-full bg-green-300 dark:bg-green-500 rounded-2xl transform transition-transform ease-in duration-500 border-green-500 dark:border-gray-800 border-2`} />
-      </label>
+      {isClient && (
+        <label className="border-green-300 dark:border-green-500 w-16 h-8 border-2 flex rounded-2xl cursor-pointer" >
+          <input 
+            type="checkbox" 
+            hidden
+            onChange={darkMode.toggle} 
+          />
+          <div className={`${darkMode.value ? 'translate-x-full' : ''} w-1/2 h-full bg-green-300 dark:bg-green-500 rounded-2xl transform transition-transform ease-in duration-500 border-green-500 dark:border-gray-800 border-2`} />
+        </label>
+      )}
     </div>
   )
 }
